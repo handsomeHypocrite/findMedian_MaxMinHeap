cd4cf5fe059d9ba3896c870a81cd1ca5d0420647
Bokai Chen <kavenchen@hotmail.co.nz>
2017-12-01 19:27:05 +1300

    Commented FindMed file and updated readme.

--- FindMed.py
+++ FindMed.py
@@ -1,19 +1,22 @@
 #Bokai Chen
 #The program uses MAX MIN heap to find the median of the array.
-#additional number can be added after initial array is created
+#additional number can be added repetitively after initial array is created
+#input format is 1,2,3,4
+#note: if size of input is even, the middle 2 numbers are averaged
 
 import heapq
 import numpy as np
 import statistics
 
-# rawInput2 = input("please enter additional array \n")
 
 def randarr():
+    """generating random test data"""
     randArr = np.random.rand(int(10 * np.random.rand(1)))
     return [int(100 * i) for i in randArr]
 
 
 def inputArr():
+    """Getting user input and convert into int list"""
     rawInput = input("please enter initial/additional array, eg: 1,2,3 \n")
     if len(rawInput) > 0:
         Array = [int(num) for num in rawInput.split(",")]
@@ -23,22 +26,17 @@ def inputArr():
 
 
 def arrInsertHeap(array, RoR):
+    """Inserting new integers into heaps"""
     global left_heap, right_heap
     for num in array:
+#RoR is the root of the right heap, this is to ensure all elements in the left heap is smaller than all elements int the right heap
         if num < RoR:
             heapq.heappush(left_heap, -num)
         else:
             heapq.heappush(right_heap, num)
 
-    # print(left_heap)
-    # print(right_heap)
-    # print(len(left_heap))
-    # print(len(right_heap))
-
-
+# Blancing the size of the 2 heaps
     adjustment = len(left_heap) - len(right_heap)
-
-    # print(adjustment)
 
     if adjustment > 0:
         for i in range(int(adjustment / 2)):
@@ -47,26 +45,21 @@ def arrInsertHeap(array, RoR):
         for i in range(int(-adjustment / 2)):
             heapq.heappush(left_heap, -heapq.heappop(right_heap))
 
-    # print(left_heap)
-    # print(right_heap)
-    # print(len(left_heap))
-    # print(len(right_heap))
-
+# Extracting median from the heaps
 def medFromHeap():
     global left_heap, right_heap
     if len(left_heap) - len(right_heap) == 0:
         median = (-left_heap[0] + right_heap[0]) / 2
-        print("both heap used")
     elif len(right_heap) > len(left_heap):
         median = right_heap[0]
-        print("right heap used")
     else:
         median = -left_heap[0]
-        print("left heap used")
 
     return median
 
 
+
+#initialization
 array = []
 left_heap = []
 right_heap = []
@@ -75,7 +68,11 @@ RoR = 0
 while True:
 
     newNumbers = inputArr()
-    newNumbers = randarr()
+
+#to test with random numbers
+    # newNumbers = randarr()
+
+
     array += newNumbers
 
     if len(right_heap)>0:
@@ -86,17 +83,10 @@ while True:
 
     median = medFromHeap()
 
-    print("length of array is: %d" %len(array))
-    print("length of left heap is: %d" %len(left_heap))
-    print("length of right heap is: %d" %len(right_heap))
+    print("calcuated median: %.1f" %median)
 
-    print(array)
-    print(left_heap)
-    print(right_heap)
+#Checking result using statistics module
+    # realMed = statistics.median(array)
+    # print("actual median: %.1f" %realMed)
 
-    realMed = statistics.median(array)
-
-
-    print("calcuated median: %d" %median)
-    print("actual median: %d" %realMed)
     print("\n")
--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Given an array of n integers. We are allowed to add k additional integer in the 
 and then find the median of the resultant array. We can choose any k values to be
 added.
 
+Interger array can be added repetitively.
+New values are pushed into heaps and median if found efficiently.
+
 The program is implented using Max Min Heap.
-Got the idea from:
+Got the BASIC idea from:
 https://stackoverflow.com/questions/11361320/data-structure-to-find-median
+
+note: if size of input is even, the middle 2 numbers are averaged
