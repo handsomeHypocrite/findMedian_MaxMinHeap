d549cc6679c63f37fab0992f65d0f83055aa13d1
Bokai Chen <kavenchen@hotmail.co.nz>
2017-12-01 18:59:21 +1300

    FindMed is now a on going function. - It processes additional input value - New values are inserted into the left and right stack. - Median of Three method is abandoned bacause the need to ensure all element in the left array is smaller than all elements in the right array.

--- FindMed.py
+++ FindMed.py
@@ -6,72 +6,97 @@ import heapq
 import numpy as np
 import statistics
 
-rawInput = input("please enter initial array, eg: 1,2,3 \n")
-rawInput2 = input("please enter additional array \n")
+# rawInput2 = input("please enter additional array \n")
+
+def randarr():
+    randArr = np.random.rand(int(10 * np.random.rand(1)))
+    return [int(100 * i) for i in randArr]
 
 
-randArr = np.random.rand(int(100*np.random.rand(1)))
-arr = [int(100*i) for i in randArr]
+def inputArr():
+    rawInput = input("please enter initial/additional array, eg: 1,2,3 \n")
+    if len(rawInput) > 0:
+        Array = [int(num) for num in rawInput.split(",")]
+    else:
+        Array = []
+    return Array
 
 
-if len(rawInput) > 0:
-    initialArray = [int(num) for num in rawInput.split(",")]
-else:
-    initialArray = []
-    MoT = False
+def arrInsertHeap(array, RoR):
+    global left_heap, right_heap
+    for num in array:
+        if num < RoR:
+            heapq.heappush(left_heap, -num)
+        else:
+            heapq.heappush(right_heap, num)
 
-if len(rawInput2)>0:
-    addArray = [int(num) for num in rawInput2.split(",")]
-else:
-    addArray = []
+    # print(left_heap)
+    # print(right_heap)
+    # print(len(left_heap))
+    # print(len(right_heap))
 
-array = initialArray + addArray
 
-array = arr
-# print(array)
+    adjustment = len(left_heap) - len(right_heap)
 
-MoT_array = sorted([array[0], array[-1], array[int(len(array) / 2)]])
-MoT = MoT_array[1]
+    # print(adjustment)
 
-# print(MoT)
+    if adjustment > 0:
+        for i in range(int(adjustment / 2)):
+            heapq.heappush(right_heap, -heapq.heappop(left_heap))
+    elif adjustment < 0:
+        for i in range(int(-adjustment / 2)):
+            heapq.heappush(left_heap, -heapq.heappop(right_heap))
 
+    # print(left_heap)
+    # print(right_heap)
+    # print(len(left_heap))
+    # print(len(right_heap))
+
+def medFromHeap():
+    global left_heap, right_heap
+    if len(left_heap) - len(right_heap) == 0:
+        median = (-left_heap[0] + right_heap[0]) / 2
+        print("both heap used")
+    elif len(right_heap) > len(left_heap):
+        median = right_heap[0]
+        print("right heap used")
+    else:
+        median = -left_heap[0]
+        print("left heap used")
+
+    return median
+
+
+array = []
 left_heap = []
 right_heap = []
+RoR = 0
 
-for num in array:
-    if num < MoT:
-        heapq.heappush(left_heap, -num)
-    else:
-        heapq.heappush(right_heap, num)
+while True:
 
-# print(left_heap)
-# print(right_heap)
-# print(len(left_heap))
-# print(len(right_heap))
+    newNumbers = inputArr()
+    newNumbers = randarr()
+    array += newNumbers
 
-
-adjustment = len(left_heap) - len(right_heap)
-
-print(adjustment)
-
-if adjustment > 0:
-    for i in range(int(adjustment/2)):
-        heapq.heappush(right_heap, -heapq.heappop(left_heap))
-elif adjustment < 0:
-    for i in range(int(-adjustment/2)):
-        heapq.heappush(left_heap, -heapq.heappop(right_heap))
+    if len(right_heap)>0:
+        RoR = right_heap[0]
 
 
-print(left_heap)
-print(right_heap)
-print(len(left_heap))
-print(len(right_heap))
-if len(left_heap) - len(right_heap) == 0:
-    median = (-left_heap[0] + right_heap[0])/2
-elif len(right_heap)>len(left_heap):
-    median = right_heap[0]
-else:
-    median = -left_heap[0]
+    arrInsertHeap(newNumbers, RoR)
 
-print("calcuated median: %d" %median)
-print("actual median: %d" %statistics.median(array))
+    median = medFromHeap()
+
+    print("length of array is: %d" %len(array))
+    print("length of left heap is: %d" %len(left_heap))
+    print("length of right heap is: %d" %len(right_heap))
+
+    print(array)
+    print(left_heap)
+    print(right_heap)
+
+    realMed = statistics.median(array)
+
+
+    print("calcuated median: %d" %median)
+    print("actual median: %d" %realMed)
+    print("\n")
